8d0b56c2d68119b556ef06f8daa702bf9ab2fd26
tign <tingh5370@gmail.com>
2023-12-02 12:51:00 +0800

    UT for token properties

--- migrations/2_deploy_token.js
+++ migrations/2_deploy_token.js
@@ -1,6 +1,6 @@
 const TGNToken = artifacts.require("TGNToken");
 
 module.exports = async function (deployer) {
-  const initialSupply = 100; // 传入的参数值
+  const initialSupply = 10000; // 传入的参数值
   await deployer.deploy(TGNToken, initialSupply, { gas: 5000000 });
 };
--- test/TGNToken.test.js
+++ test/TGNToken.test.js
@@ -1,18 +1,34 @@
 const TGNToken = artifacts.require("TGNToken");
 
-const assert = require("chai").assert;
+require("chai").use(require("chai-as-promised")).should();
 
 contract("TGNToken ERC20 Token Test", ([owner, receiver, exchange]) => {
   let token;
 
   beforeEach(async () => {
-    token = await TGNToken.deployed(10000);
+    token = await TGNToken.new(10000);
   });
 
-  describe("Token properties test", () => {
+  describe("Token deployment", () => {
     it("Test name", async () => {
       const name = await token.name();
-      assert.equal(name, "Tign");
+      name.should.equal("Tign");
+    });
+    it("Test symbol", async () => {
+      const symbol = await token.symbol();
+      symbol.should.equal("TGN");
+    });
+    it("Test decimal", async () => {
+      const decimals = await token.decimals();
+      decimals.toString().should.equal("18");
+    });
+    it("Test name", async () => {
+      const totalSupply = await token.totalSupply();
+      totalSupply.toString().should.equal("10000");
+    });
+    it("Test balance", async () => {
+      const balance = await token.balanceOf(owner);
+      balance.toString().should.equal("10000");
     });
   });
 });
