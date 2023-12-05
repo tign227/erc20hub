b2e1d70175514404a8e7971a1b1b8b08b5672223
tign <tingh5370@gmail.com>
2023-12-05 15:50:37 +0800

    deposit and withdraw Ethers

---
+++ contracts/Exchange.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import "./TGNToken.sol";
+
+contract Exchange {
+    // TODO:
+    // [X] Set the fee account
+    // [X] Deposit Ether 对应 depositEther
+    // [X] Withdraw Ether
+    // [X] Deposit tokens 对应 depositToken
+    // [X] Withdraw tokens
+    // [X] Check balances
+    // [X] Make order
+    // [X] Cancel order
+    // [X] Fill order
+    // [X] Charge fees
+    address public feeAccount;
+    uint256 public feePercentage;
+    address constant ETHER = address(0);
+    //token address => user address => total
+    mapping(address => mapping(address => uint256)) public tokens;
+
+    event Deposit(
+        address _token,
+        address _owner,
+        uint256 _amount,
+        uint256 _balance
+    );
+
+    event Withdraw(
+        address _token,
+        address _owner,
+        uint256 _amount,
+        uint256 _balance
+    );
+
+    constructor(address _feeAccount, uint _feePercentage) {
+        feeAccount = _feeAccount;
+        feePercentage = _feePercentage;
+    }
+
+    function depositEther() public payable {
+        tokens[ETHER][msg.sender] = tokens[ETHER][msg.sender] + msg.value;
+        emit Deposit(ETHER, msg.sender, msg.value, tokens[ETHER][msg.sender]);
+    }
+
+    function withdrawEther(uint256 _amount) public {
+        require(tokens[ETHER][msg.sender] >= _amount);
+        tokens[ETHER][msg.sender] = tokens[ETHER][msg.sender] - _amount;
+        payable(msg.sender).transfer(_amount);
+        emit Withdraw(ETHER, msg.sender, _amount, tokens[ETHER][msg.sender]);
+    }
+
+    function depositToken(address _token, uint256 _amount) public {
+        require(_token != ETHER);
+        require(
+            TGNToken(_token).transferFrom(msg.sender, address(this), _amount)
+        );
+        tokens[_token][msg.sender] = tokens[_token][msg.sender] + _amount;
+        emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+    }
+
+    function withdrawToken(address _token, uint256 _amount) public {
+        require(_token != ETHER);
+        require(tokens[_token][msg.sender] >= _amount);
+        tokens[_token][msg.sender] = tokens[_token][msg.sender] - _amount;
+        require(TGNToken(_token).transfer(msg.sender, _amount));
+        emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+    }
+
+    function balanceOf(
+        address _token,
+        address _owner
+    ) public view returns (uint256) {
+        return tokens[_token][_owner];
+    }
+}
---
+++ test/Exchange.test.js
@@ -0,0 +1,106 @@
+const { should } = require("chai");
+const ETHER_ADDRESS = "0x0000000000000000000000000000000000000000";
+const Exchange = artifacts.require("./Exchange");
+const Token = artifacts.require("./TGNToken");
+
+require("chai").use(require("chai-as-promised")).should();
+
+contract(
+  "ERC20 Token Exchange Test",
+  ([deployer, feeAccount, etherUser, tokenUser]) => {
+    let exchange;
+    const feePercentage = 10;
+
+    beforeEach(async () => {
+      exchange = await Exchange.new(feeAccount, feePercentage);
+    });
+
+    describe("Exchange deployment", () => {
+      it("Test the fee account", async () => {
+        const account = await exchange.feeAccount();
+        account.toString().should.equal(feeAccount);
+      });
+
+      it("Test the fee percentage", async () => {
+        const percentage = await exchange.feePercentage();
+        percentage.toString().should.equal(feePercentage.toString());
+      });
+    });
+
+    describe("Deposit Ether", async () => {
+      let result;
+      let depositAmount;
+
+      beforeEach(async () => {
+        depositAmount = 100;
+        result = await exchange.depositEther({
+          from: etherUser,
+          value: depositAmount,
+        });
+      });
+
+      it("Test on Ether deposit", async () => {
+        const balance = await exchange.tokens(ETHER_ADDRESS, etherUser);
+        balance.toString().should.equal(depositAmount.toString());
+      });
+
+      it("Test on Deposit event", async () => {
+        const log = result.logs[0];
+        log.event.should.equal("Deposit");
+
+        const args = log.args;
+        args._token.should.equal(ETHER_ADDRESS);
+        args._owner.should.equal(etherUser);
+        args._amount.toString().should.equal(depositAmount.toString());
+        args._balance.toString().should.equal(depositAmount.toString());
+      });
+    });
+
+    describe("Withdraw Ether", async () => {
+      let result;
+      let depositAmount;
+      let withdrawAmount;
+
+      beforeEach(async () => {
+        depositAmount = 100;
+        withdrawAmount = depositAmount;
+        result = await exchange.depositEther({
+          from: etherUser,
+          value: depositAmount,
+        });
+      });
+
+      describe("success", async () => {
+        beforeEach(async () => {
+          result = await exchange.withdrawEther(withdrawAmount, {
+            from: etherUser,
+          });
+        });
+
+        it("Test on Ether withdraw", async () => {
+          const balance = await exchange.tokens(ETHER_ADDRESS, etherUser);
+          balance.toString().should.equal("0");
+        });
+
+        it("Test on Ether Withdraw event", async () => {
+          const log = result.logs[0];
+          log.event.should.equal("Withdraw");
+
+          const args = log.args;
+          args._token.should.equal(ETHER_ADDRESS);
+          args._owner.should.equal(etherUser);
+          args._amount.toString().should.equal(withdrawAmount.toString());
+          args._balance.toString().should.equal("0");
+        });
+      });
+
+      describe("failure", async () => {
+        it("Test withdraw with insuffient balance", async () => {
+          const overWithdrawAmount = withdrawAmount + 1;
+          await exchange.withdrawEther(overWithdrawAmount, { from: etherUser })
+            .should.be.rejected;
+        });
+      });
+    });
+  }
+);
